e587aafe2878f182eeafd89be62dbf3032714343
Adebimpe Islamiyyah Adefila <95176308+MiyyahCodes@users.noreply.github.com>
2022-05-13 18:55:02 +0100

    updated files

--- app.js
+++ app.js
@@ -10,3 +10,27 @@ function prev() {
   index = (index - 1 + slides.length) % slides.length;
   slides[index].classList.add("active");
 }
+(function () {
+  "use strict";
+  var items = document.querySelectorAll(".timeline li");
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
+    }
+  }
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
